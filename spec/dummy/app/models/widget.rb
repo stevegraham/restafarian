71670ff4c17c7a11a0e1d1d7be68e9ac905d8f24
Stevie Graham <sjtgraham@mac.com>
2014-04-23 02:39:18 +0100

    JavascriptRender: Length validator

--- spec/dummy/app/models/widget.rb
+++ spec/dummy/app/models/widget.rb
@@ -3,12 +3,12 @@ class Widget < ActiveRecord::Base
   validates :integer, numericality: { only_integer: true }
   validates :terms, acceptance: true
   validates :favourite_colour, inclusion: { in: %w<red green blue>}
-  validates :password, presence: true
+  validates :password, presence: true, length: { minimum: 8, maximum: 32 }
   validates :doo_dad,  absence: true
 
   attr_accessor :cover_photo, :main_image, :chat_avatar, :profile_picture,
                 :resume_file, :password, :password_confirmation,
-                :telephone_number, :phone_number, :blog_url, :lucky_number,
+                :telephone_number, :phone_number, :lucky_number,
                 :favourite_colour, :doo_dad
 
   def as_json(options={})
--- spec/js/validation_spec.rb
+++ spec/js/validation_spec.rb
@@ -1,36 +1,53 @@
 require 'spec_helper'
 
 describe 'JavaScript Validators:' do
-  let(:renderer) { Restafarian::JavascriptRenderer.new Widget.new }
-  let(:context)  { V8::Context.new.tap { |c| c.eval(renderer.render) } }
-  let(:resource) { context[:Resource] }
-  let(:errors)   { resource[:validate].methodcall(resource,representation) }
+  let(:renderer)       { Restafarian::JavascriptRenderer.new Widget.new }
+  let(:context)        { V8::Context.new.tap { |c| c.eval(renderer.render) } }
+  let(:resource)       { context[:Resource] }
+  let(:errors)         { resource[:validate].methodcall(resource,representation) }
+  let(:representation) { Hash[Widget.new.as_json.map { |k,v| [k, ''] }] }
 
   describe 'presence' do
     context 'with valid input' do
-      let(:representation) { { password: 'crappy_password_bro' } }
-
+      before  { representation['password'] = 'crappy_password_bro' }
       specify { expect(errors[:password].to_a).to be_empty }
     end
 
     context 'with invalid input' do
-      let(:representation) { { password: '' } }
-
       specify { expect(errors[:password].to_a).to include("can't be blank") }
     end
   end
 
   describe 'absence' do
     context 'with valid input' do
-      let(:representation) { { doo_dad: '' } }
-
       specify { expect(errors[:doo_dad].to_a).to be_empty }
     end
 
     context 'with invalid input' do
-      let(:representation) { { doo_dad: 'hello' } }
-
+      before  { representation['doo_dad'] = 'hello' }
       specify { expect(errors[:doo_dad].to_a).to include('must be blank') }
+    end
+  end
+
+  describe 'length' do
+    context 'too long' do
+      before  { representation['password'] =  'qwertyuiopasdfghjklzxcvbnm1234567' }
+
+      specify { expect(errors[:password].to_a).
+        to include('is too long (maximum is 32 characters)') }
+    end
+
+    context 'too short' do
+      before  { representation['password'] =  'hello' }
+
+      specify { expect(errors[:password].to_a).
+        to include('is too short (minimum is 8 characters)') }
+    end
+
+    context 'with valid input' do
+      before  { representation['password'] = 'hello_world' }
+
+      specify { expect(errors[:password].to_a).to be_empty }
     end
   end
 end
